41f04d40c64a72bcc4b9cd1acdae1e0a583d4b7d
Arthur Picard <arthur.picard7@gmail.com>
2022-09-22 14:03:12 +0200

    App & Stocklist component created, remove useless docs

---
+++ src/components/StockList.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const StockList = () => {
+  return (
+    <>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Chg</th>
+          <th>Chg%</th>
+          <th>High</th>
+          <th>Low</th>
+        </tr>
+        <tr>
+          <td>Name</td>
+          <td>Price</td>
+          <td>Chg</td>
+          <td>Chg%</td>
+          <td>High</td>
+          <td>Low</td>
+        </tr>
+
+      </table>
+    </>
+  )
+}
+
+export default StockList
