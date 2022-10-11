26abaae25883f66703a265a0f9c2737cd901d591
Arthur Picard <arthur.picard7@gmail.com>
2022-10-12 00:05:53 +0200

    Trending coin card created and working (need to be move to components)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-// import { useQuery } from '';
+import { useQuery } from '@tanstack/react-query'
 import { fetchTrending } from '../api/dataService';
 
 import './Home.scss';
 
 const Home = () => {
+  const { data: trending } = useQuery(['trending'], fetchTrending)
   return (
     <div className='home-container'>
       <div id="trending-container">
@@ -12,10 +13,19 @@ const Home = () => {
         <span>Trending</span>
         <div className="trending-table">
           <table>
-            <ul className='trending-table-coin'>
-              <li className='trending-coin-item'>Bitcoin</li>
-              <li className='trending-coin-item'>20245 €</li>
-            </ul>
+            <tbody>
+              {trending?.coins.map((coin, id) =>
+                <ul key={id} className='trending-table-coin'>
+                  <img className='trending-coin-img' src={`${coin.item.small}`} alt={`${coin.item.id}`} />
+                  <div className="trending-coin-item">
+                    <li>{coin.item.id}</li>
+                    <li>{coin.item.symbol}</li>
+                    <li>{coin.item.price_btc.toFixed(4)}</li>
+                  </div>
+                </ul>
+
+              )}
+            </tbody>
           </table>
         </div>
       </div>
