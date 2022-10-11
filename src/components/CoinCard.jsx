ce844a5aba1a0b648e1b5aa0aaec5ca9fe261211
Arthur Picard <arthur.picard7@gmail.com>
2022-10-12 00:34:19 +0200

    basic coin card component created

---
+++ src/components/CoinCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './scss/CoinCard.scss';
+
+const CoinCard = () => {
+  return (
+    <div className='coin-container'>
+      <div className="coin-infos">
+        <img src="" alt="" />
+        <span>btc</span>
+        <span>bitcoin</span>
+        <span>19056€</span>
+      </div>
+    </div>
+  )
+}
+
+export default CoinCard
