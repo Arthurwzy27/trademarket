1724b8217967ff43ad19a0f7bda79d0e61513b52
Arthur Picard <arthur.picard7@gmail.com>
2022-10-12 17:52:31 +0200

    Clean up addToFav function

--- src/components/AllCoins.jsx
+++ src/components/AllCoins.jsx
@@ -16,48 +16,14 @@ const AllCoins = () => {
     setFavorites(coin)
   }
 
-  console.log(favorites);
-
-  // ---TEST
   const addToFav = (coin, copy) => {
-
     if(favorites.indexOf(coin) !== -1) {
-      // --------- JUST ADD NOW .id to item and when already in fav, if I search for it I can't put it back again and it renders the all lis again
       const newFavs = favorites.filter(item => item.id !== coin)
-      console.log('log IF newFavs:', newFavs)
-
-      // ---------- Not logging because when favorite, coin removed from list so can't re-add
-      console.log(`${coin.name} is already in your favorites`)
       setFavorites(newFavs)
     } else {
       const newFavs = favorites.filter(item => item.id !== coin.id)
       setFavorites([...newFavs, coin])
-      // setSearchCoin('')
-      // console.log(`addToFav -> ${coin.id} in ElseIf`, favorites)
     }
-
-    // const alreadyInFav = favorites
-          // .filter(fav => fav.id !== id)
-          // .filter(item => item.id !== id)
-          // console.log(alreadyInFav)
-
-    // if (favorites.includes(id)) {
-    //   const newFav = favorites.filter(fav => fav !== id)
-    //   setFavorites(...favorites, newFav)
-    //   console.log('---- favorite include id', favorites.includes(id))
-    //   console.log('******log newFav:', newFav)
-    // } else { setFavorites([...favorites, id])}
-    // if (alreadyInFav) {
-    //   console.log(`/! ${id.name} /! already in`)
-    //   && setFavorites([...favorites])
-    // } else {
-    //   console.log(`adding ***${id.name}*** into the list`)
-    //   && setFavorites([...favorites, id])
-    // }
-    // const data = listOfCoin.find(item => item.id === id);
-    // setFavorites([...favorites, id])
-    // setListOfCoin(listOfCoin)
-
   }
 
   const deleteFav = id => {
